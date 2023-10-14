e5153b2de0e640bb7141d4ad2a754f763619c7b5
DimaSudenkov <sudenkovdimas@gmail.com>
2023-10-14 16:35:47 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const selectors = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]')
+}
+selectors.stop.disabled = true;
 
+selectors.start.addEventListener('click', handlerClickStart);
+selectors.stop.addEventListener('click', handlerClickStop)
+
+function handlerClickStart(event) {
+  timerId = setInterval(() => {
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    }
+    selectors.body.style.backgroundColor = getRandomHexColor()
+  }, 1000)
+  event.currentTarget.disabled = true
+  selectors.stop.disabled = false
+}
+
+function handlerClickStop(event) {
+  clearInterval(timerId);
+  event.currentTarget.disabled = true
+  selectors.start.disabled = false
+}
