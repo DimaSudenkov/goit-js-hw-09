fb0462002108882cbdd066081fcd905867c9b635
DimaSudenkov <134227414+DimaSudenkov@users.noreply.github.com>
2023-10-16 18:26:16 +0000

    Deploying to gh-pages from @ DimaSudenkov/goit-js-hw-09@c94f4359642fd967c61c0a35d3c85af340466bad 🚀

--- 01-color-switcher.13c6b740.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={body:document.querySelector("body"),start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};t.stop.disabled=!0,t.start.addEventListener("click",(function(){timerId=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.start.disabled=!0,t.stop.disabled=!1})),t.stop.addEventListener("click",(function(){clearInterval(timerId),t.stop.disabled=!0,t.start.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.13c6b740.js.map
