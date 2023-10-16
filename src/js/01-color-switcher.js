9c778e2d580ba4c43643b3b3132a42c089d1935d
DimaSudenkov <sudenkovdimas@gmail.com>
2023-10-16 21:15:42 +0300

    rest task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,7 +6,7 @@ const selectors = {
 selectors.stop.disabled = true;
 
 selectors.start.addEventListener('click', handlerClickStart);
-selectors.stop.addEventListener('click', handlerClickStop)
+selectors.stop.addEventListener('click', handlerClickStop);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
@@ -15,13 +15,13 @@ function getRandomHexColor() {
 function handlerClickStart() {
   timerId = setInterval(() => {
     selectors.body.style.backgroundColor = getRandomHexColor()
-  }, 1000)
-  selectors.start.disabled = true
-  selectors.stop.disabled = false
+  }, 1000);
+  selectors.start.disabled = true;
+  selectors.stop.disabled = false;
 }
 
 function handlerClickStop() {
   clearInterval(timerId);
-  selectors.stop.disabled = true
-  selectors.start.disabled = false
+  selectors.stop.disabled = true;
+  selectors.start.disabled = false;
 }
